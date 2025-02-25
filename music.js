fc17bc78411332ee86588afed5c48af1fc20ab05
Jeffrey-Adjei <118208940+Jeffrey-Adjei@users.noreply.github.com>
2025-02-25 20:45:17 +0000

    created music js file

---
+++ music.js
@@ -0,0 +1,10 @@
+const playlist = [
+    {
+       name: "Nikes",
+       path: "./assets/audio/Nikes.mp3", 
+    },
+    {
+        name: "Ivy",
+        path: "./assets/audio/Ivy.mp3",
+    }
+]
--- script.js
+++ script.js
@@ -3,24 +3,4 @@ let time = document.querySelector(".time");
 let btnPlay = document.querySelector(".play");
 let btnPause = document.querySelector(".pause");
 let btnPrev = document.querySelector(".prev");
-let btnNext = document.querySelector(".next");
-
-const playlist = [
-    'Nikes.mp3',
-    'Ivy.mp3',
-    'Pink + White',
-    'Be Yourself.mp3',
-    'Solo.mp3',
-    'Skyline.mp3',
-    'Self Control.mp3',
-    'Good Guy.mp3',
-    'Nights.mp3',
-    'Solo (Reprise).mp3',
-    'Pretty Sweet.mp3',
-    'Facebook Story.mp3',
-    'Close To You.mp3',
-    'White Ferrari.mp3',
-    'Seigfried.mp3',
-    'Godspeed.mp3',
-    'Futura Free',
-];
+let btnNext = document.querySelector(".next"); 
